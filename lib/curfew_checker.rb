4f43dc0a93b03a70d89ed4150e8bb731a9e03575
angelinasongco <angelinasongco123@gmail.com>
2018-07-11 05:26:21 +0000

    automatically backed up by learn

--- lib/curfew_checker.rb
+++ lib/curfew_checker.rb
@@ -36,6 +36,6 @@ end
 
 puts deluxe_curfew_checker (9)
 
-# # def platinum_curfew_checker(current_time, curfew_time)
-# #   # code goes here
-# # end
+def platinum_curfew_checker(current_time, curfew_time)
+  # code goes her
+end
