f5f9b7bd49e63afb3ea391df7ae84e4b4d7be10d
angelinasongco <angelinasongco123@gmail.com>
2018-07-11 05:15:34 +0000

    automatically backed up by learn

--- lib/curfew_checker.rb
+++ lib/curfew_checker.rb
@@ -1,19 +1,39 @@
 def simple_curfew_checker(time)
-  # code goes here
+  if time == 11
+    "You're in trouble! Better get home quick!"
+  elsif time > 11
+    "You're in trouble! Better get home quick!"
+  end  
 end
 
-def curfew_checker(time)
-  # code goes here
-end
+puts simple_curfew_checker(12)
+puts simple_curfew_checker(13)
+
 
 def complex_curfew_checker(time)
-  # code goes here
+  if time == 11
+    "Time to apparate!"
+  elsif time > 11
+    "You're in trouble! Better get home quick!"
+  else
+    "Keep having fun!"
+  end  
 end
+
+puts complex_curfew_checker(12)
 
 def deluxe_curfew_checker(time)
-  # code goes here
+  if time == 11
+    "Time to apparate!"
+  elsif time > 11
+    "You're in trouble! Better get home quick!"
+  else  
+  curfew = 11  
+  puts "You have " + time-curfew.to_s  + " hour(s) left to keep having fun!"
+  end  
 end
 
-def platinum_curfew_checker(current_time, curfew_time)
-  # code goes here
-end
+puts deluxe_curfew_checker (9)
+# # def platinum_curfew_checker(current_time, curfew_time)
+# #   # code goes here
+# # end
